2072237ce3a914201b3dd886b3bcaee459bf5621
Renz Edwin Austria <austria.renzedwinv@gmail.com>
2023-12-03 01:37:24 +0800

    new update

--- openai_mp3_generator.js
+++ openai_mp3_generator.js
@@ -10,11 +10,28 @@ async function main() {
   const mp3 = await openai.audio.speech.create({
     model: "tts-1-hd",
     voice: "nova",
-    input: "kumusta",
+    input: "ku mus ta",
     speed: 0.7,
+    content_type: "audio/mpeg",
   });
   console.log(speechFile);
   const buffer = Buffer.from(await mp3.arrayBuffer());
   await fs.promises.writeFile(speechFile, buffer);
+
+  // try {
+  //   const mp3 = await openai.audio.speech.create({
+  //     model: "tts-1-hd",
+  //     voice: "nova",
+  //     input: "kumusta",
+  //     speed: 0.7,
+  //     content_type: "audio/mpeg",
+  //   });
+
+  //   const buffer = Buffer.from(await mp3.arrayBuffer());
+  //   await fs.promises.writeFile(speechFile, buffer);
+  //   console.log("MP3 file generated successfully:", speechFile);
+  // } catch (error) {
+  //   console.error("Error generating MP3:", error);
+  // }
 }
 main();
