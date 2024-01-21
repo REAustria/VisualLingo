f78f758e3cf8f3c9b5a3738d084241cb9a12c4ae
Renz Gerald Austria <renzgeraldv.austria@gmail.com>
2024-01-21 17:05:50 +0800

    final change

--- openai_mp3_generator.js
+++ openai_mp3_generator.js
@@ -9,7 +9,7 @@ const speechFile = path.resolve("./kumusta.mp3");
 async function main() {
   const opus = await openai.audio.speech.create({
     model: "tts-1-hd",
-    voice: "onyx",
+    voice: "nova",
     input: "ku mus ta",
     speed: 0.7,
     content_type: "audio/mpeg",
